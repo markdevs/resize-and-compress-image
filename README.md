1f6285cb305d99c576b7e861e09f1d204504cd70
Marcos de Barros Azevedo <mbarrosazevedo@gmail.com>
2022-04-08 00:26:02 -0300

    resize  and compress

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # resize-and-compress-image
- Redimensionando e Comprimindo Imagem
+
+Redimensionando e Comprimindo Imagem passando os argumentos pelo terminal
+
+# Como utilizar?
+
+Adicione uma imagem na pasta raiz do projeto
+No terminal insira o seguinte comando:
+
+```
+node app nome-da-imagem tamanho
+
+```
+
+ex: node app img-test.jpg 500
---
+++ app.js
@@ -0,0 +1,63 @@
+const sharp = require('sharp');
+const compress_images = require("compress-images");
+
+let path = process.argv[2];
+let width = Number(process.argv[3]);
+
+function resize(inputPath, outputPath, width) {
+    sharp(inputPath).resize({
+            width: width
+        })
+        .toFile(outputPath, (err) => {
+            if (err) {
+                console.log(err);
+            }
+            else {
+                console.log('Imagem Redimensionada');
+                compress(outputPath, './compressed/');
+            }
+        });
+}
+
+
+
+
+
+function compress(pathInput, outputPath) {
+    
+    compress_images(pathInput, outputPath, {
+            compress_force: false,
+            statistic: true,
+            autoupdate: true
+        }, false, {
+            jpg: {
+                engine: "mozjpeg",
+                command: ["-quality", "60"]
+            }
+        }, {
+            png: {
+                engine: "pngquant",
+                command: ["--quality=20-50", "-o"]
+            }
+        }, {
+            svg: {
+                engine: "svgo",
+                command: "--multipass"
+            }
+        }, {
+            gif: {
+                engine: "gifsicle",
+                command: ["--colors", "64", "--use-col=web"]
+            }
+        },
+        function (error, completed, statistic) {
+            console.log("-------------");
+            console.log(error);
+            console.log(completed);
+            console.log(statistic);
+            console.log("-------------");
+        }
+    );
+}
+
+resize(path, './temp/output_img_resize.jpg' ,width);
